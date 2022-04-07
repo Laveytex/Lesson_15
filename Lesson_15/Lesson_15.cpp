b73c6795f800f3ee3f9f5e0c07555f238d8ccd75
Laveytex <YanAlex13@mail.ru>
2022-04-07 10:02:03 +0400

    Com_01
    Separate function

--- Lesson_15/Lesson_15.cpp
+++ Lesson_15/Lesson_15.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
 
+void FindOddEvenNumbers(int FirstNumder, int LastNumbmber, int NumberType)
+{
+
+    FirstNumder += FirstNumder % 2 * NumberType;
+    for (int i = FirstNumder; i < LastNumbmber; i = i + 2)
+    {
+        std::cout << i << "\n";
+    }
+
+}
+
 int main()
 {
-    
-    int FirstNumdet = 0;
+    int FirstNumder = 0;
     int LastNumbmber = 0;
     int NumberType=0;
+
     std::cout << "Write range number from a to b:\n";
-    std::cin >> FirstNumdet;
+    std::cin >> FirstNumder;
     std::cin >> LastNumbmber;
     std::cout << "Enter 0 or 1 corresponding to odd or even:\n";
     std::cin >> NumberType;
-    /*
-    for (int i = 0; i < number; i++)
-    {
-        a = i-i*(i%2);
-        std::cout << a;      
-      
-    }
-    */
-    FirstNumdet += FirstNumdet % 2 * NumberType;
-    for (int i = FirstNumdet; i < LastNumbmber; i=i+2)
-    {
-        
-        std::cout << i<< "\n";
-        /*
-        if (i % 2 == 0)
-         {
-           std::cout << i;
-         }
-        */
 
-    }
+    FindOddEvenNumbers(FirstNumder, LastNumbmber, NumberType);
 
+    return 0;
 }
 
-void FindOddNumbers(int Limit, bool IsOdd)
-{
-    
-}
+
+
